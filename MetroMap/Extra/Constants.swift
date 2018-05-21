a45af40e49fde8bee5d9347d3d092c7d72c8a524
Roman <misnikovroman@gmail.com>
2018-05-21 22:31:51 +0300

    Added blue and red lines of SPb metropolitan (big matrix) + Alamofire

---
+++ MetroMap/Extra/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MetroMap
+//
+//  Created by Роман Мисников on 17.05.2018.
+//  Copyright © 2018 Роман Мисников. All rights reserved.
+//
+
+import Foundation
+
+// url constants
+let URL_METRO_MAP = "https://raw.githubusercontent.com/MisnikovRoman/MetroMap/master/JSON/metro_map_v1.json"
---
+++ Pods/Target Support Files/Pods-MetroMapTests/Pods-MetroMapTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MetroMapTests : NSObject
+@end
+@implementation PodsDummy_Pods_MetroMapTests
+@end
