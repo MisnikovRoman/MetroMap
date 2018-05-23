b8bff579018bd6d4333c391dbfbaf909766c7df8
Roman <misnikovroman@gmail.com>
2018-05-23 17:09:34 +0300

    Edit json v2

--- MetroMap/Controllers/SimpleVC.swift
+++ MetroMap/Controllers/SimpleVC.swift
@@ -29,17 +29,15 @@ class SimpleVC: UIViewController {
         guard let url = URL(string: URL_METRO_MAP) else { return }
         Alamofire.request(url).responseJSON { (response) in
 
-            print(response.error)
-            
-//            // if there is no error
-//            guard response.error == nil else { return }
-//            // try to convert received data to dictionary
-//            guard let json = response.result.value as? Dictionary<String, Any> else { return }
-//            // try get data
-//            guard let data = response.data else { return }
-//
-//            guard let metroMapModel = try? JSONDecoder().decode(MetroModel.self, from: data) else { return }
-//            self.metroModel = metroMapModel
+            // if there is no error
+            guard response.error == nil else { return }
+            // try to convert received data to dictionary
+            guard let json = response.result.value as? Dictionary<String, Any> else { return }
+            // try get data
+            guard let data = response.data else { return }
+
+            guard let metroMapModel = try? JSONDecoder().decode(MetroModel.self, from: data) else { return }
+            self.metroModel = metroMapModel
 
         }
     }
